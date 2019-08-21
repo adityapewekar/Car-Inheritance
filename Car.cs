a478a57490f7b6333ca5f0c767a2faf6c53ec5fd
ameya kherodkar <ameyack@gmail.com>
2019-08-20 21:49:02 -0400

    Mileage Corrected

--- Car.cs
+++ Car.cs
@@ -10,34 +10,35 @@ namespace CarInheritance
     {
         public bool isSedan;
         public string seats;
-        public string carName;
+        // public string carName;
 
-        public Car(bool isSedan,string seats)
+        public Car(bool isSedan, string seats, int Mileage)
         {
-            Console.Write(" is "+(isSedan?"":"not")+" Sedan, is " + seats + " - seater,");
+            Console.Write(" is " + (isSedan ? "" : "not") + " Sedan, is " + seats + " - seater,");
         }
 
         public bool getIsSedan() {
             return isSedan;
         }
 
-        public string getSeats()
-        {
+        public string getSeats(){
             return seats;
         }
 
-        public abstract string getMileage(int mileage);
+        public abstract string getMileage();
     }
 
     public class WagonR : Car
     {
-        public WagonR(bool isSedan,string seats):base(isSedan,seats)
+        int mileage;
+        public WagonR(bool isSedan,string seats,int Mileage): base(isSedan,seats,Mileage)
         {
-            base.carName = "WagonR";
+            mileage = Mileage;
+           // base.carName = "WagonR";
             //Console.WriteLine("A WagonR is not Sedan, is" + seats + "-seater, and has a mileage of around 22 kmpl");
         }
 
-        public override string getMileage(int mileage)
+        public override string getMileage()
         {
             return "and has a mileage of around " + mileage.ToString() + " kmpl";
         }
@@ -45,13 +46,15 @@ namespace CarInheritance
 
     public class HondaCity : Car
     {
-        public HondaCity(bool isSedan, string seats) : base(isSedan, seats)
+        int mileage;
+        public HondaCity(bool isSedan, string seats, int Mileage) : base(isSedan, seats,Mileage)
         {
-            base.carName = "Honda City";
+            mileage = Mileage;
+            //base.carName = "Honda City";
             //Console.WriteLine("A Honda City is a Sedan, is" + seats + "-seater, and has a mileage of around 22 kmpl");
         }
 
-        public override string getMileage(int mileage)
+        public override string getMileage()
         {
             return "and has a mileage of around " + mileage.ToString() + " kmpl";
         }
@@ -59,13 +62,15 @@ namespace CarInheritance
 
     public class InnovaCrysta : Car
     {
-        public InnovaCrysta(bool isSedan, string seats) : base(isSedan, seats)
+        int mileage;
+        public InnovaCrysta(bool isSedan, string seats, int Mileage) : base(isSedan, seats,Mileage)
         {
-            base.carName = "Innova Crysta";
+            mileage = Mileage;
+            //base.carName = "Innova Crysta";
             //Console.WriteLine("A Innova Crysta is not Sedan, is" + seats + "-seater, and has a mileage of around 22 kmpl");
         }
 
-        public override string getMileage(int mileage)
+        public override string getMileage()
         {
             return "and has a mileage of around "+mileage.ToString()+" kmpl";
         }
